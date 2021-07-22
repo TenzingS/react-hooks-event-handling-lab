79a10f7d348b0be3aeef7b5b6c8cd735a121e340
TenzingS <thesherpatenzing@gmail.com>
2021-07-22 11:59:51 -0400

    Completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+function Keypad(){
+    return (
+        <input 
+            type="password" 
+            placeholder = "Enter Password"
+            onChange={() => console.log('Entering password...')} />
+    )
+}
+
+export default Keypad;
